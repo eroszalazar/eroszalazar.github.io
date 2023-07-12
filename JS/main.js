eb7412d8ad602cef3add804df7e572df1fbabb93
Eros Zalazar <124445666+eroszalazar@users.noreply.github.com>
2023-07-12 19:26:22 -0300

    Initial commit

---
+++ JS/main.js
@@ -0,0 +1,6 @@
+function enviarCorreo() {
+    window.location.href = 'mailto:eroszalazar@gmail.com';
+  }
+  function redirigirCalendly() {
+    window.location.href = 'https://calendly.com/eroszalazar';
+  }
